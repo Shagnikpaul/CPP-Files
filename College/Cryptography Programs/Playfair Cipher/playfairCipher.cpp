7a5a7f6a9f0aaafe077770a57bf92580b1279635
shagnikpaul <shagnkpaul.772@gmail.com>
2026-01-19 19:58:23 +0530

    completed playfair implementation

--- College/Cryptography Programs/Playfair Cipher/playfairCipher.cpp
+++ College/Cryptography Programs/Playfair Cipher/playfairCipher.cpp
@@ -55,6 +55,10 @@ int main()
         cout << endl;
     }
 
+    // fixing I/J as same cell in the matrix...
+    row['J' - 'A'] = row['I' - 'A'];
+    col['J' - 'A'] = col['I' - 'A'];
+
     /*
         plain - text pre-processing :
         - remove spaces
@@ -81,7 +85,7 @@ int main()
             processedPT += plainText[i];
         }
     }
-
+    cout << "Stage 1 processing plain text : " << processedPT << endl;
     /*
         - insert 'X' in between repeated characters
         - add 'X' at the end if odd length
@@ -89,24 +93,94 @@ int main()
 
     string processedPT2;
 
-    for (int i = 0; i <= processedPT.length() - 1; i = i + 2)
+    for (int i = 0; i < processedPT.length();)
     {
         processedPT2 += processedPT[i];
-        if (processedPT[i] == processedPT[i + 1])
+        if (i + 1 < processedPT.length())
         {
-            processedPT2 += 'X';
+            if (processedPT[i] == processedPT[i + 1])
+            {
+                processedPT2 += 'X';
+                i++;
+            }
+            else
+            {
+                processedPT2 += processedPT[i + 1];
+                i += 2;
+            }
         }
-        processedPT2 += processedPT[i + 1];
+        else
+        {
+            i++;
+        }
     }
-    if (processedPT2.length() % 2 == 0)
+    if (processedPT2.length() % 2 != 0)
     {
         processedPT2 += 'X';
     }
 
-    cout << "After all the processing the plain text becomes : " << processedPT2;
+    cout << "Stage 2 processing plain text : " << processedPT2 << endl;
 
-    for (int i = 0; i <= processedPT2.length() - 1; i = i + 2)
+    // encryption
+
+    string cipherText = "";
+    for (int i = 0; i < processedPT2.length() - 1; i = i + 2)
     {
+        int r1 = row[processedPT2[i] - 'A'];
+        int r2 = row[processedPT2[i + 1] - 'A'];
+        int c1 = col[processedPT2[i] - 'A'];
+        int c2 = col[processedPT2[i + 1] - 'A'];
+
+        if (r1 == r2)
+        {
+            cipherText += (char)keyMatrix[r1][(c1 + 1) % 5];
+            cipherText += (char)keyMatrix[r1][(c2 + 1) % 5];
+        }
+        else if (c1 == c2)
+        {
+            cipherText += (char)keyMatrix[(r1 + 1) % 5][c2];
+            cipherText += (char)keyMatrix[(r2 + 1) % 5][c2];
+        }
+        else
+        {
+            cipherText += (char)keyMatrix[r1][c2];
+            cipherText += (char)keyMatrix[r2][c1];
+        }
     }
+    cout << "After encryption process the cipher text we get : " << cipherText << endl;
+
+    // decryption ...
+    string decryptedPlainText = "";
+    for (int i = 0; i < cipherText.length() - 1; i = i + 2)
+    {
+        int r1 = row[cipherText[i] - 'A'];
+        int r2 = row[cipherText[i + 1] - 'A'];
+        int c1 = col[cipherText[i] - 'A'];
+        int c2 = col[cipherText[i + 1] - 'A'];
+
+        if (r1 == r2)
+        {
+            decryptedPlainText += (char)keyMatrix[r1][(c1 - 1) % 5 + ((c1 - 1) % 5 < 0 ? 5 : 0)];
+            decryptedPlainText += (char)keyMatrix[r1][(c2 - 1) % 5 + ((c2 - 1) % 5 < 0 ? 5 : 0)];
+        }
+        else if (c1 == c2)
+        {
+
+            decryptedPlainText += (char)keyMatrix[(r1 - 1) % 5 + ((r1 - 1) % 5 < 0 ? 5 : 0)][c2];
+            decryptedPlainText += (char)keyMatrix[(r2 - 1) % 5 + ((r2 - 1) % 5 < 0 ? 5 : 0)][c2];
+        }
+        else
+        {
+            decryptedPlainText += (char)keyMatrix[r1][c2];
+            decryptedPlainText += (char)keyMatrix[r2][c1];
+        }
+    }
+    string finalDecryptedPlainText = "";
+    for (char x : decryptedPlainText)
+    {
+        if (x != 'X')
+            finalDecryptedPlainText += x;
+    }
+    cout << "After decryption process the final plain text we get : " << finalDecryptedPlainText << endl;
     return 0;
 }
